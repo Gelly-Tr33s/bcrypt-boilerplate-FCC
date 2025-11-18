32c366e24f292168dc34add0bd07f4c9c2461814
Gelly-Tr33s <nfgel007@gmail.com>
2025-11-18 17:54:40 +0800

    Added Synchronous hash and compare passwords

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-    console.log(hash);
-    bcrypt.compare(myPlaintextPassword, hash, (err, res)  => {
-        console.log(res);
-    })
-});
+//bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+//    console.log(hash);
+//    bcrypt.compare(myPlaintextPassword, hash, (err, res)  => {
+//        console.log(res);
+//    })
+//});
 
 
 //END_ASYNC
